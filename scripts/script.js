bde8c05bc1e88089a9af364ddcf01981d0582256
Alisha Soedamah <alisha.soedamah@gmail.com>
2026-03-23 18:14:53 +0100

    all imgs in the same place

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
 window.onload = () => {
   const cursors = [
-    "/images/earth_32x32.png",
-    "/images/mars_32x32.png",
-    "/images/venus_32x32.png",
-    "/images/jupiter_32x32.png",
-    "/images/saturn_32x32.png",
-    "/images/uranus_32x32.png",
-    "/images/mercury_32x32.png",
-    "/images/neptune_32x32.png"
+    "public/images/earth_32x32.png",
+    "public/images/mars_32x32.png",
+    "public/images/venus_32x32.png",
+    "public/images/jupiter_32x32.png",
+    "public/images/saturn_32x32.png",
+    "public/images/uranus_32x32.png",
+    "public/images/mercury_32x32.png",
+    "public/images/neptune_32x32.png"
   ];
 
   const random = cursors[Math.floor(Math.random() * cursors.length)];
